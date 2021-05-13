4ceabfb19b3b760a728248706b64c8ee45656912
Mikail Fidan <fdnmikail@gmail.com>
2021-05-14 00:28:00 +0300

    Update testOrder.js

--- testOrder.js
+++ testOrder.js
@@ -1,1 +1,57 @@
 
+[{
+  "id" : "1",
+  "title" : "Affenpinscher",      
+  "breed" : "Affenpinscher",
+  "colour":"Red",
+  "category":"Find",
+  "location":"Ankara",
+  "desc":"LThe Affenpinscher: loyal, curious, and famously amusing; this almost-human toy dog is fearless ou…",
+  "photos":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/30174107/Affenpinscher-copy.jpg",
+  "date":"21.01.2021"
+},
+{
+  "id" : "2",
+  "title" : "Afghan Hound ",      
+  "breed" : "Afghan Hound",
+  "colour":"Black",
+  "category":"Lost",
+  "location":"Mardin",
+  "desc":"The Afghan Hound is an aloof and dignified aristocrat of sublime beauty. Despite his regal…. The point of using Lorem Ipsum ",
+  "photos":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/26151212/Afghan-Hound-standing-in-a-garden.jpg",
+  "date":"13.06.2021"
+},
+{
+  "id" : "3",
+  "title" : "American Bulldogs",      
+  "breed" : "Bombay",
+  "colour":"Red",
+  "location":"Ankara",
+  "category":"Find",
+  "desc":"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum ",
+  "photos":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2020/01/14144341/American-Bulldog-standing-in-three-quarter-view.jpg",
+  "date":"30.05.2021"
+},
+ {
+  "id" : "4",
+  "title" : "Alaskan Klee Kai ",      
+  "breed" : "Alaskan Klee Kai ",
+  "colour":"White",
+   "category":"Lost",
+  "location":"Van",
+  "desc":"The Alaskan Klee Kai is a small-sized companion dog that is alert, energetic, and curiou. The point of using Lorem Ipsum",
+  "photos":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2021/01/29143827/Alaskan-Klee-Kai-standing-in-the-snow-attached-to-a-harness.jpg",
+  "date":"09.08.2021"
+},
+ {
+  "id" : "5",
+  "title" : "American Leopard Hound",      
+  "breed" : "Regdoll",
+  "colour":"Grey",
+  "category":"Lost",
+  "location":"Istanbul",
+  "desc":"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum ",
+  "photos":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001556/American-Leopard-Hound-On-White-01.jpg",
+  "date":"01.09.2021" 
+}
+ ]
